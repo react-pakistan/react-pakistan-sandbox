9bd3894b33f1a57933f5b1e7a5247e95f78d601c
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-10-24 09:16:30 +0500

    :recycle: added react ui component

--- src/react-ui.js
+++ src/react-ui.js
@@ -8,6 +8,7 @@ import {
   Spacer,
   Text
 } from '@react-pakistan/react-commons-collection';
+import { CheckoutFinish, SectionDetail, BLOCK_TYPE } from '@react-pakistan/react-ui-collection';
 
 export const ReactUI = withTheme(({ theme }) => (
   <Section backgroundColor={theme.colors.skyBlue}>
@@ -22,12 +23,42 @@ export const ReactUI = withTheme(({ theme }) => (
       <HeadingWrapper>
         <H2>EcommerceApp</H2>
         <Spacer padding={theme.spacing.large} />
-        <App1Button>App1 Button</App1Button>
+        <CheckoutFinish
+          mainHeadingH4='Checkout'
+          steps={[{ id: 'Delivery', stepLabel: 'Delivery', }, { id: 'Confirmation', stepLabel: 'Confirmation' }, { id: 'Finish', stepLabel: 'Finish' }]}
+        />
       </HeadingWrapper>
       <HeadingWrapper>
         <H2>PortfolioApp</H2>
         <Spacer padding={theme.spacing.large} />
-        <App2Button>App2 Button</App2Button>
+        <SectionDetail blockType={BLOCK_TYPE.REGULAR} detailBlocks={[  {
+    id: 'block-1',
+    yearStarted: '2017',
+    yearEnded: 'Present',
+    designation: 'Senior Software Engineer',
+    companyName: 'Facebook Inc',
+  },
+  {
+    id: 'block-2',
+    yearStarted: '2014',
+    yearEnded: '2017',
+    designation: 'Mid Software Engineer',
+    companyName: 'Google Inc',
+  },
+  {
+    id: 'block-3',
+    yearStarted: '2011',
+    yearEnded: '2014',
+    designation: 'Junior Software Engineer',
+    companyName: 'Microsoft Corporation',
+  },
+  {
+    id: 'block-4',
+    yearStarted: '2011',
+    yearEnded: '2014',
+    designation: 'Junior Software Engineer',
+    companyName: 'Microsoft Corporation',
+  },]} />
       </HeadingWrapper>
     </Grid>
   </Section>
